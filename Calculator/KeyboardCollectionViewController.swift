2a66b9eb14e409efe220df5fb7eca1051f942cd2
Avi Cohen <cohena100@gmail.com>
2016-07-24 12:11:08 +0300

    refactored KeyboardCollectionViewController to use a mediator

--- Calculator/KeyboardCollectionViewController.swift
+++ Calculator/KeyboardCollectionViewController.swift
@@ -10,43 +10,16 @@ import UIKit
 
 class KeyboardCollectionViewController: NSObject {
     
-    let calculatorCommands: CalculatorCommands
     let keyboardCollectionView: KeyboardCollectionView
-    var isLandscape: Bool
     weak var view: UIView!
-    var acOrCIndex: (portrait: Int, landscape: Int)!
-    var dataSourcePortrait: KeyboardKeyElements
-    var dataSourceLandscape: KeyboardKeyElements
+    let mediator = KeyboardCollectionViewMediator()
     
     init(keyboardCollectionView: KeyboardCollectionView, view: UIView) {
-        var path = NSBundle.mainBundle().pathForResource("PortraitElements", ofType: "plist")!
-        var arr = NSArray(contentsOfFile: path) as! [AnyObject]
-        dataSourcePortrait = KeyboardKeyElements(arr: arr)
-        path = NSBundle.mainBundle().pathForResource("Themes", ofType: "plist")!
-        let dict = NSDictionary(contentsOfFile: path) as! [NSObject: AnyObject]
-        let theme = dict["theme"] as! String
-        path = NSBundle.mainBundle().pathForResource(theme, ofType: "plist")!
-        arr = NSArray(contentsOfFile: path) as! [AnyObject]
-        dataSourceLandscape = KeyboardKeyElements(arr: arr)
-        calculatorCommands = Model.sharedInstance.factory.getCalculatorCommands()
         self.keyboardCollectionView = keyboardCollectionView
         self.keyboardCollectionView.registerNib(UINib(nibName: "KeyCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "key")
         self.keyboardCollectionView.registerNib(UINib(nibName: "DisplayCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "display")
-        self.isLandscape = false
         self.view = view
         super.init()
-        for i in 0 ..< dataSourcePortrait.elements.count {
-            if dataSourcePortrait.elements[i].action == CalculatorCommands.Action.c || dataSourcePortrait.elements[i].action == CalculatorCommands.Action.ac {
-                acOrCIndex = (landscape: 0, portrait: i)
-                break
-            }
-        }
-        for i in 0 ..< dataSourceLandscape.elements.count {
-            if dataSourceLandscape.elements[i].action == CalculatorCommands.Action.c || dataSourceLandscape.elements[i].action == CalculatorCommands.Action.ac {
-                acOrCIndex = (landscape: i, portrait: acOrCIndex.portrait)
-                break
-            }
-        }
         self.keyboardCollectionView.delegate = self
         self.keyboardCollectionView.dataSource = self
     }
@@ -56,7 +29,7 @@ class KeyboardCollectionViewController: NSObject {
     }
     
     func didRotate(isLandscape: Bool) {
-        self.isLandscape = isLandscape
+        mediator.isLandscape = isLandscape
         self.keyboardCollectionView.reloadData()
     }
     
@@ -65,43 +38,11 @@ class KeyboardCollectionViewController: NSObject {
 extension KeyboardCollectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        let displayHeight = (1.0 / 6.0) * view.frame.height
-        switch indexPath.section {
-        case 0:
-            return CGSize(width: view.frame.width, height: displayHeight)
-        default:
-            var keyWidth: CGFloat
-            var keyHeight: CGFloat
-            let element: KeyboardKeyElement
-            if isLandscape {
-                element = dataSourceLandscape.elements[indexPath.row]
-                keyWidth = (view.frame.width - 9) / 10
-                keyHeight = (view.frame.height - displayHeight - 1 - 4) / 5
-            } else {
-                element = dataSourcePortrait.elements[indexPath.row]
-                keyWidth = (view.frame.width - 3) / 4
-                keyHeight = (view.frame.height - displayHeight - 1 - 4) / 5
-            }
-            if element.action == CalculatorCommands.Action.zero {
-                keyWidth = keyWidth * 2 + 1
-            }
-            return CGSize(width: keyWidth, height: keyHeight)
-        }
+        return mediator.sizeForItemAtIndexPath(indexPath, viewFrame: view.frame)
     }
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        switch indexPath.section {
-        case 0:
-            return
-        default:
-            let element = isLandscape ? dataSourceLandscape.elements[indexPath.row] : dataSourcePortrait.elements[indexPath.row]
-            guard let action = element.action else {
-                return
-            }
-            calculatorCommands.keyAction(action)
-            dataSourcePortrait.elements[acOrCIndex.portrait] = KeyboardKeyElement(action: calculatorCommands.acOrC, color: dataSourcePortrait.elements[acOrCIndex.portrait].color)
-            dataSourceLandscape.elements[acOrCIndex.landscape] = KeyboardKeyElement(action: calculatorCommands.acOrC, color: dataSourceLandscape.elements[acOrCIndex.landscape].color)
-            let itemsToReload = isLandscape ? [NSIndexPath(forRow: 0, inSection: 0), NSIndexPath(forRow: acOrCIndex.landscape, inSection: 1)] : [NSIndexPath(forRow: 0, inSection: 0), NSIndexPath(forRow: acOrCIndex.portrait, inSection: 1)]
+        if let itemsToReload = mediator.didSelectItemAtIndexPath(indexPath) {
             keyboardCollectionView.reloadItemsAtIndexPaths(itemsToReload)
         }
     }
@@ -111,23 +52,18 @@ extension KeyboardCollectionViewController: UICollectionViewDelegateFlowLayout {
 extension KeyboardCollectionViewController: UICollectionViewDataSource {
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 1
-        default:
-            return isLandscape ? dataSourceLandscape.elements.count : dataSourcePortrait.elements.count
-        }
+        return mediator.numberOfItemsInSection(section)
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        switch indexPath.section {
-        case 0:
-            let cell = collectionView.dequeueReusableCellWithReuseIdentifier("display", forIndexPath: indexPath) as! DisplayCollectionViewCell
-            cell.numberLabel.text = calculatorCommands.display
+        let cellType = mediator.cellForItemAtIndexPath(indexPath)
+        switch cellType {
+        case .display(let id, let text):
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(id, forIndexPath: indexPath) as! DisplayCollectionViewCell
+            cell.numberLabel.text = text
             return cell
-        default:
-            let cell = collectionView.dequeueReusableCellWithReuseIdentifier("key", forIndexPath: indexPath) as! KeyCollectionViewCell
-            let element = isLandscape ? dataSourceLandscape.elements[indexPath.row] : dataSourcePortrait.elements[indexPath.row]
+        case .key(let id, let element):
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(id, forIndexPath: indexPath) as! KeyCollectionViewCell
             cell.backgroundColor = element.color
             cell.symbolLabel.text = element.action?.rawValue
             return cell
@@ -135,6 +71,6 @@ extension KeyboardCollectionViewController: UICollectionViewDataSource {
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 2
+        return mediator.numberOfSections()
     }
 }
---
+++ Calculator/KeyboardCollectionViewMediator.swift
@@ -0,0 +1,114 @@
+//
+//  KeyboardCollectionViewMediator.swift
+//  Calculator
+//
+//  Created by Avi Cohen on 24/7/16.
+//  Copyright © 2016 Avi Cohen. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardCollectionViewMediator {
+    
+    enum CellType {
+        case display(id: String, text: String)
+        case key(id: String, element: KeyboardKeyElement)
+    }
+    
+    var acOrCIndex: (portrait: Int, landscape: Int)!
+    var dataSourcePortrait: KeyboardKeyElements
+    var dataSourceLandscape: KeyboardKeyElements
+    let calculatorCommands: CalculatorCommands
+    var isLandscape = false
+    
+    init() {
+        var path = NSBundle.mainBundle().pathForResource("PortraitElements", ofType: "plist")!
+        var arr = NSArray(contentsOfFile: path) as! [AnyObject]
+        dataSourcePortrait = KeyboardKeyElements(arr: arr)
+        path = NSBundle.mainBundle().pathForResource("Themes", ofType: "plist")!
+        let dict = NSDictionary(contentsOfFile: path) as! [NSObject: AnyObject]
+        let theme = dict["theme"] as! String
+        path = NSBundle.mainBundle().pathForResource(theme, ofType: "plist")!
+        arr = NSArray(contentsOfFile: path) as! [AnyObject]
+        dataSourceLandscape = KeyboardKeyElements(arr: arr)
+        calculatorCommands = Model.sharedInstance.factory.getCalculatorCommands()
+        for i in 0 ..< dataSourcePortrait.elements.count {
+            if dataSourcePortrait.elements[i].action == CalculatorCommands.Action.c || dataSourcePortrait.elements[i].action == CalculatorCommands.Action.ac {
+                acOrCIndex = (landscape: 0, portrait: i)
+                break
+            }
+        }
+        for i in 0 ..< dataSourceLandscape.elements.count {
+            if dataSourceLandscape.elements[i].action == CalculatorCommands.Action.c || dataSourceLandscape.elements[i].action == CalculatorCommands.Action.ac {
+                acOrCIndex = (landscape: i, portrait: acOrCIndex.portrait)
+                break
+            }
+        }
+    }
+    
+    func sizeForItemAtIndexPath(indexPath: NSIndexPath, viewFrame frame: CGRect) -> CGSize {
+        let displayHeight = (1.0 / 6.0) * frame.height
+        switch indexPath.section {
+        case 0:
+            return CGSize(width: frame.width, height: displayHeight)
+        default:
+            var keyWidth: CGFloat
+            var keyHeight: CGFloat
+            let element: KeyboardKeyElement
+            if isLandscape {
+                element = dataSourceLandscape.elements[indexPath.row]
+                keyWidth = (frame.width - 9) / 10
+                keyHeight = (frame.height - displayHeight - 1 - 4) / 5
+            } else {
+                element = dataSourcePortrait.elements[indexPath.row]
+                keyWidth = (frame.width - 3) / 4
+                keyHeight = (frame.height - displayHeight - 1 - 4) / 5
+            }
+            if element.action == CalculatorCommands.Action.zero {
+                keyWidth = keyWidth * 2 + 1
+            }
+            return CGSize(width: keyWidth, height: keyHeight)
+        }
+    }
+    
+    func didSelectItemAtIndexPath(indexPath: NSIndexPath) -> [NSIndexPath]? {
+        switch indexPath.section {
+        case 0:
+            return nil
+        default:
+            let element = isLandscape ? dataSourceLandscape.elements[indexPath.row] : dataSourcePortrait.elements[indexPath.row]
+            guard let action = element.action else {
+                return nil
+            }
+            calculatorCommands.keyAction(action)
+            dataSourcePortrait.elements[acOrCIndex.portrait] = KeyboardKeyElement(action: calculatorCommands.acOrC, color: dataSourcePortrait.elements[acOrCIndex.portrait].color)
+            dataSourceLandscape.elements[acOrCIndex.landscape] = KeyboardKeyElement(action: calculatorCommands.acOrC, color: dataSourceLandscape.elements[acOrCIndex.landscape].color)
+            let itemsToReload = isLandscape ? [NSIndexPath(forRow: 0, inSection: 0), NSIndexPath(forRow: acOrCIndex.landscape, inSection: 1)] : [NSIndexPath(forRow: 0, inSection: 0), NSIndexPath(forRow: acOrCIndex.portrait, inSection: 1)]
+            return itemsToReload
+        }
+    }
+    
+    func numberOfItemsInSection(section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        default:
+            return isLandscape ? dataSourceLandscape.elements.count : dataSourcePortrait.elements.count
+        }
+    }
+    
+    func cellForItemAtIndexPath(indexPath: NSIndexPath) -> CellType {
+        switch indexPath.section {
+        case 0:
+            return CellType.display(id: "display", text: calculatorCommands.display)
+        default:
+            let element = isLandscape ? dataSourceLandscape.elements[indexPath.row] : dataSourcePortrait.elements[indexPath.row]
+            return CellType.key(id: "key", element: element)
+        }
+        
+    }
+    
+    func numberOfSections() -> Int {
+        return 2
+    }
+}
